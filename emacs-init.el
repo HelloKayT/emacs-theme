77fb9130a152099ba7bba584afe62826fdae33d9
HelloKayT <katiemlim@gmail.com>
2015-09-22 15:16:06 -0400

    Adding xterm-256 color variant

--- emacs-init.el
+++ emacs-init.el
@@ -24,6 +24,19 @@
   (set-face-foreground 'font-lock-warning-face "#cc0000")
 )
 
+(defun katie-monokai-256-color-theme()
+  (set-face-foreground 'font-lock-preprocessor-face "#008700")
+  (set-face-foreground 'font-lock-builtin-face "#ff5fdf")
+  (set-face-foreground 'font-lock-comment-face "#444444")
+  (set-face-foreground 'font-lock-constant-face "#4e9a06")
+  (set-face-foreground 'font-lock-function-name-face "#5fd700")
+  (set-face-foreground 'font-lock-keyword-face "#ff00df")
+  (set-face-foreground 'font-lock-string-face "#e6db74")
+  (set-face-foreground 'font-lock-type-face "#00afd7")
+  (set-face-foreground 'font-lock-variable-name-face "#af87ff")
+  (set-face-foreground 'font-lock-warning-face "#cc0000")
+)
+
 (add-hook 'font-lock-mode-hook 'katie-monokai-color-theme)
 
 (defun set-katie-window-attributes ()
@@ -49,6 +62,33 @@
 
 
 (set-katie-window-attributes)
+
+;still working on coloring the background
+(defun set-katie-256-window-attributes ()
+  (when window-system
+    (set-frame-position (selected-frame) 0 0)
+    (cond ((= (display-pixel-height) 1080)
+           (set-frame-height (selected-frame) 66)
+           (set-frame-width (selected-frame) 112)
+           (set-frame-font "9x15bold"))
+          (t
+           (set-frame-height (selected-frame) 48)
+           (set-frame-width (selected-frame) 112)
+           (set-frame-font "9x15bold")))
+    (set-foreground-color "#1c1c1c")
+    (set-background-color "#1c1c1c")
+    (set-face-foreground 'default "#c0c0c0")
+    (set-face-background 'default "#c0c0c0")
+    ;;alternate highlighting color: #75715E
+    (set-face-background 'region "#5f005f")
+    (set-face-foreground 'region "#5f005f")
+    (set-face-foreground 'mode-line "black")
+    (set-face-background 'mode-line "lightgrey")
+    (set-face-foreground 'menu "black")
+    (set-face-background 'menu "lightgrey")
+    (set-face-background 'cursor "#afeeee")))
+
+;(set-katie-window-attributes)
 
 
 
