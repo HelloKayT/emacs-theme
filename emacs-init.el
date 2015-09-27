b943c5052ea53acc3f910e70f367b1b4bb59e99e
HelloKayT <katiemlim@gmail.com>
2015-09-27 03:20:40 -0400

    Changing highlight color for 256 color theme. Not working on background color for emacs in 256 theme, because I changed it manually in putty

--- emacs-init.el
+++ emacs-init.el
@@ -27,7 +27,7 @@
 (defun katie-monokai-256-color-theme()
   (set-face-foreground 'font-lock-preprocessor-face "#008700")
   (set-face-foreground 'font-lock-builtin-face "#ff5fdf")
-  (set-face-foreground 'font-lock-comment-face "#444444")
+  (set-face-foreground 'font-lock-comment-face "#626262")
   (set-face-foreground 'font-lock-constant-face "#4e9a06")
   (set-face-foreground 'font-lock-function-name-face "#5fd700")
   (set-face-foreground 'font-lock-keyword-face "#ff00df")
@@ -35,6 +35,8 @@
   (set-face-foreground 'font-lock-type-face "#00afd7")
   (set-face-foreground 'font-lock-variable-name-face "#af87ff")
   (set-face-foreground 'font-lock-warning-face "#cc0000")
+  (set-face-foreground 'minibuffer-prompt "#8787ff")
+  (set-face-background 'region "#5f0087")
 )
 
 (add-hook 'font-lock-mode-hook 'katie-monokai-color-theme)
@@ -63,7 +65,8 @@
 
 (set-katie-window-attributes)
 
-;still working on coloring the background
+
+;;not sure what colors the background, manually changed background in putty
 (defun set-katie-256-window-attributes ()
   (when window-system
     (set-frame-position (selected-frame) 0 0)
@@ -80,15 +83,13 @@
     (set-face-foreground 'default "#c0c0c0")
     (set-face-background 'default "#c0c0c0")
     ;;alternate highlighting color: #75715E
-    (set-face-background 'region "#5f005f")
+    ;;(set-face-background 'region "#5f005f")
     (set-face-foreground 'region "#5f005f")
     (set-face-foreground 'mode-line "black")
     (set-face-background 'mode-line "lightgrey")
     (set-face-foreground 'menu "black")
     (set-face-background 'menu "lightgrey")
     (set-face-background 'cursor "#afeeee")))
-
-;(set-katie-window-attributes)
 
 
 
